17b8211ca375fdd563d9b3e9145123be4f787de5
Frank <frank.hulley-jones@guardian.co.uk>
2016-11-18 14:23:51 +0000

    daniel arwas

--- common/app/views/support/FiveADayHelpers.scala
+++ common/app/views/support/FiveADayHelpers.scala
@@ -7,31 +7,31 @@ case class FiveADayItem(imageSrc: String, articleUrl: String, kicker: String, st
 object FiveADayHelpers {
   def getItems: Seq[FiveADayItem] = Seq(
     FiveADayItem(
-      Static("images/five/nicholas-fonsing/sidebar_number_1.png"),
+      Static("images/five/daniel-arwas/sidebar_number_1.png"),
       "/film/2016/sep/01/arrival-review-amy-adams-denis-villeneuve-alien-contact",
       "Arrival Review",
       "Amy Adams has a sublime word with alien visitors"
     ),
     FiveADayItem(
-      Static("images/five/nicholas-fonsing/sidebar_number_2.png"),
+      Static("images/five/daniel-arwas/sidebar_number_2.png"),
       "/lifeandstyle/2016/nov/16/fish-fingers-taste-test-which-get-the-thumbs-up",
       "Taste tests",
       "Which fish fingers gets the thumbs up?"
     ),
     FiveADayItem(
-      Static("images/five/nicholas-fonsing/sidebar_number_3.png"),
+      Static("images/five/daniel-arwas/sidebar_number_3.png"),
       "/technology/2016/nov/17/elon-musk-satellites-internet-spacex",
       "Elon Musk",
       "wants to cover the world wth internet from space"
     ),
     FiveADayItem(
-      Static("images/five/nicholas-fonsing/sidebar_number_4.png"),
-      "/us-news/2016/nov/17/hillary-clinton-first-public-appearance-bernie-sanders",
-      "Clinton’",
-      "'There have been times when I wanted never to leave the house again'"
+      Static("images/five/daniel-arwas/sidebar_number_4.png"),
+      "/football/blog/2016/nov/18/premier-league-10-things-to-look-out-for-this-weekend",
+      "Premier League’",
+      "10 things to look out for this weekend"
     ),
     FiveADayItem(
-      Static("images/five/nicholas-fonsing/sidebar_number_5.png"),
+      Static("images/five/daniel-arwas/sidebar_number_5.png"),
       "/technology/shortcuts/2016/nov/14/mobile-smartphones-health-silly-walk",
       "The 'phone walk'",
       "the next stage in human evolution"
