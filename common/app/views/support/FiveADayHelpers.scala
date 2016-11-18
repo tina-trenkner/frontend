3f5f068681defe5a4b5b01f71eb2040823422792
Frank <frank.hulley-jones@guardian.co.uk>
2016-11-18 10:55:28 +0000

    george barnes

--- common/app/views/support/FiveADayHelpers.scala
+++ common/app/views/support/FiveADayHelpers.scala
@@ -7,34 +7,34 @@ case class FiveADayItem(imageSrc: String, articleUrl: String, kicker: String, st
 object FiveADayHelpers {
   def getItems: Seq[FiveADayItem] = Seq(
     FiveADayItem(
-      Static("images/five/melvin-rose/sidebar_number_1.png"),
+      Static("images/five/george-barnes/sidebar_number_1.png"),
       "/film/2016/sep/01/arrival-review-amy-adams-denis-villeneuve-alien-contact",
       "Arrival Review",
       "Amy Adams has a sublime word with alien visitors"
     ),
     FiveADayItem(
-      Static("images/five/melvin-rose/sidebar_number_2.png"),
+      Static("images/five/george-barnes/sidebar_number_2.png"),
       "/lifeandstyle/2016/nov/16/fish-fingers-taste-test-which-get-the-thumbs-up",
       "Taste tests",
       "Which fish fingers gets the thumbs up?"
     ),
     FiveADayItem(
-      Static("images/five/melvin-rose/sidebar_number_3.png"),
-      "/music/2016/nov/17/still-kicking-it-a-tribe-called-quest",
-      "A Tribe Called Quest return",
-      "‘You fight for what you love – and you go through hell to get it’"
+      Static("images/five/george-barnes/sidebar_number_3.png"),
+      "/technology/2016/nov/17/elon-musk-satellites-internet-spacex",
+      "Elon Musk",
+      "wants to cover the world wth internet from space"
     ),
     FiveADayItem(
-      Static("images/five/melvin-rose/sidebar_number_4.png"),
+      Static("images/five/george-barnes/sidebar_number_4.png"),
       "/football/blog/2016/nov/18/premier-league-10-things-to-look-out-for-this-weekend",
       "Premier League’",
       "10 things to look out for this weekend"
     ),
     FiveADayItem(
-      Static("images/five/melvin-rose/sidebar_number_5.png"),
-      "/artanddesign/2016/nov/13/design-museum-kensington-new-building-oma-john-pawson-commonwealth",
-      "The Design Museum review",
-      "A magnificent achievement, but…"
+      Static("images/five/george-barnes/sidebar_number_5.png"),
+      "/technology/shortcuts/2016/nov/14/mobile-smartphones-health-silly-walk",
+      "The 'phone walk'",
+      "the next stage in human evolution"
     )
   )
 }
